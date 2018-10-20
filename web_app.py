bd99e9cfd46de59530132d493aa3c295af242b43
Mustafa <mful7998@uni.sydney.edu.au>
2018-10-20 22:28:21 +1100

    HTML templates added, code changed a bit

--- web_app.py
+++ web_app.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 app.secret_key = 'Be5tp@sswrdever1sHeRe'
 app.debug = True
 
+session = {}
+
 # main page
 @app.route("/")
 def hello():
-    return "Hello world"
+    return render_template("index.html")
 
 # run the application on 0.0.0.0, port 10000
 app.run(debug= True, host="0.0.0.0", port="10000")
