43cebd31cd7d60adf9a9b73f8f236a33e51bd1d3
Mustafa <mful7998@uni.sydney.edu.au>
2018-10-20 21:13:34 +1100

    Initial database setup of pg8000 done

---
+++ database.py
@@ -0,0 +1,33 @@
+import pg8000
+import configparser
+import sys
+
+# connect to the database
+def database_connect():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    connection = None
+    try:
+        connection = pg8000.connect(database=config['DATABASE']['username'],
+                                    user=config['DATABASE']['username'],
+                                    password=config['DATABASE']['password'],
+                                    host=config['DATABASE']['host'])
+    except pg8000.OperationalError as o:
+        print("ERROR: Update your config.ini, or check your wifi with correct VPN details")
+        print(o)
+        exit(-1)
+    except pg8000.ProgrammingError as p:
+        print("ERROR: Check your password and username in the config.ini file")
+        print(p)
+        exit(-1)
+    except Exception as e:
+        print("ERROR: Some error occurred!")
+        print(e)
+        exit(-1)
+
+    return connection
+
+# initial testing
+conn = database_connect()
+cur = conn.cursor()
